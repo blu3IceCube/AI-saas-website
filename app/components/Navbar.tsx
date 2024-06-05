69a6138fd2ca89610e5c332e940c42f82dc7d459
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-06-05 19:36:30 +0530

    final style touch ups

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="flex items-center w-4/5 my-3 py-3 px-6 rounded-3xl bg-fafafa/[0.4]">
+      <div className="flex items-center w-4/5 my-3 py-3 px-6 rounded-3xl bg-neutral-50/[0.4] shadow-md shadow-[#1d7cf2]">
         <div className="flex items-center gap-6 grow">
-          <h1 className="text-2xl font-normal">Voxify.ai</h1>
+          <h1 className="text-2xl font-medium text-[#1d7cf2]">Voxify.ai</h1>
           <ul className="flex items-center gap-3">
             <li>Features</li>
             <li>Testimonials</li>
