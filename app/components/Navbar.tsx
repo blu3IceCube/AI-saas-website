4741d93238d7059fbbc8b6c6c687699bab0d92f3
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-06-05 19:18:26 +0530

    adding nav styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="flex items-center w-4/5 my-3">
-        <h1 className="text-2xl font-normal">Voxify.ai</h1>
-        <div>
+      <div className="flex items-center w-4/5 my-3 py-3 px-6 rounded-3xl bg-fafafa/[0.4]">
+        <div className="flex items-center gap-6 grow">
+          <h1 className="text-2xl font-normal">Voxify.ai</h1>
           <ul className="flex items-center gap-3">
             <li>Features</li>
             <li>Testimonials</li>
