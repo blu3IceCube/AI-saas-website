3fb1347cd927949a16c49aceaa519ce98e19f00b
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-06-05 19:42:00 +0530

    fixing styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="flex items-center w-4/5 my-3 py-3 px-6 rounded-3xl bg-neutral-50/[0.4] shadow-md shadow-[#1d7cf2]">
+      <div
+        className="flex items-center w-4/5 my-3 py-3 px-6 rounded-3xl bg-neutral-50/[0.4]"
+        style={{
+          boxShadow:
+            "0 4px 6px -1px rgb(29 124 242 / 0.1), 0 2px 4px -2px rgb(29 124 242 / 0.1)",
+        }}
+      >
         <div className="flex items-center gap-6 grow">
           <h1 className="text-2xl font-medium text-[#1d7cf2]">Voxify.ai</h1>
           <ul className="flex items-center gap-3">
