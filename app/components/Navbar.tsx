6042d6835d4ed939c041d948ad0ea8cb42cb420d
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-06-05 18:50:02 +0530

    adding navbar skeleton

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex items-center justify-center">
+      <div className="flex items-center w-4/5 my-3">
+        <h1 className="text-2xl font-normal">Voxify.ai</h1>
+        <div>
+          <ul className="flex items-center gap-3">
+            <li>Features</li>
+            <li>Testimonials</li>
+            <li>Highlights</li>
+            <li>Pricing</li>
+            <li>FAQ</li>
+          </ul>
+        </div>
+        <div className="flex items-center gap-3">
+          <div>light/dark</div>
+          <div>auth</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
